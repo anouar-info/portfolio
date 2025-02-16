47c857677e92eaa1ad943335beb21edf24431d29
anouar_faraji <faraji.anouarr@gmail.com>
2025-02-16 17:56:03 +0100

    I think I finished configuring and creating the skeleton of the project

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
